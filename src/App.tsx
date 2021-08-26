3ac285f534e17c53b6ab07e4306e848b617caa9f
Evandro-Cant <evandro.souza@smarkio.com.br>
2021-08-26 10:47:04 -0300

    feat: add global.css

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/Button";
+import './styles/global.css';
+
+function App() {
+  return (
+    <div>
+      hello world
+      <Button></Button>
+    </div>
+  );
+}
+
+export default App;
