130c087302c474c479c7aa8225f5f9e4d7c5b6ca
Evandro-Cant <evandro.souza@smarkio.com.br>
2021-08-27 09:54:03 -0300

    Next

---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/pages/Profile.module.css'
+
+export function Profile() {
+    return(
+        <div className={styles.profileContainer}>
+            <img src="https://avatars.githubusercontent.com/u/66844968?v=4" alt="Evandro Cantelmo"></img>
+            <div>
+                <strong>Evandro Cantelmo</strong>
+                <p>
+                    <img src='icons/level.svg' alt=''></img>
+                    Level 1</p>
+            </div>
+        </div>
+
+    )
+}
